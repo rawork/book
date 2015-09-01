54919d4f7680b7ac7223c6b64490061c1f8a6683
Roman Alyakritskiy <rawork@yandex.ru>
2015-09-01 12:35:46 +0300

    all pages

---
+++ bundles/public/js/app.js
@@ -0,0 +1,36 @@
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel({
+            wrap: 'both'
+        });
+
+        //$('.slider-wrapper>.jcarousel').jcarouselAutoscroll({
+        //    interval: 5000
+        //});
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+
+    });
+
+})(jQuery);
